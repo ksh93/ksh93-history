5d5bb12fa9ba683d4ec0a7b2d7fb2c9f6fb9073a
Johnothan King <johnothanking@protonmail.com>
2006-12-11 13:10:34 -0500

    Version: 2006-12-11 ksh93s
    Sourced from ast-open-history version 2006-12-11 and:
    3c8e8de02004ba198ec4238a4ccd3abf99e7cedde8ce8c9e83db564d6fdc272c  INIT.2006-02-14.tgz

--- src/cmd/ksh93/sh/io.c
+++ src/cmd/ksh93/sh/io.c
@@ -18,6 +18,7 @@
 *                                                                      *
 ***********************************************************************/
 #pragma prototyped
+
 /*
  * Input/output file processing
  *
@@ -84,10 +85,221 @@ static int	(*fdnotify)(int,int);
 #         define pipe(v) ((socketpair(AF_UNIX,SOCK_STREAM,0,v)<0||shutdown((v)[0],1)<0||shutdown((v)[1],0)<0)?(-1):0)
 #      endif
 #   endif
-    static int str2inet(const char*, struct sockaddr_in*);
-#   define SOCKET	1
+
+#if !_lib_getaddrinfo
+
+#undef	EAI_SYSTEM
+
+#define EAI_SYSTEM		1
+
+#undef	addrinfo
+#undef	getaddrinfo
+#undef	freeaddrinfo
+
+#define addrinfo		local_addrinfo
+#define getaddrinfo		local_getaddrinfo
+#define freeaddrinfo		local_freeaddrinfo
+
+struct addrinfo
+{
+        int			ai_flags;
+        int			ai_family;
+        int			ai_socktype;
+        int			ai_protocol;
+        socklen_t		ai_addrlen;
+        struct sockaddr*	ai_addr;
+        struct addrinfo*	ai_next;
+};
+
+static int
+getaddrinfo(const char* node, const char* service, const struct addrinfo* hint, struct addrinfo **addr)
+{
+	unsigned long	    	ip_addr = 0;
+	unsigned short	    	ip_port = 0;
+	struct addrinfo*	ap;
+	struct hostent*		hp;
+	struct sockaddr_in*	ip;
+	char*			prot;
+	long			n;
+	
+	if (!(hp = gethostbyname(node)) || hp->h_addrtype!=AF_INET || hp->h_length>sizeof(struct in_addr))
+	{
+		errno = EADDRNOTAVAIL;
+		return EAI_SYSTEM;
+	}
+	ip_addr = (unsigned long)((struct in_addr*)hp->h_addr)->s_addr;
+	if ((n = strtol(service, &prot, 10)) > 0 && n <= USHRT_MAX && !*prot)
+		ip_port = htons((unsigned short)n);
+	else
+	{
+		struct servent*	sp;
+		const char*	protocol = 0;
+
+		if (hint)
+			switch (hint->ai_socktype)
+			{
+			case SOCK_STREAM:
+				switch (hint->ai_protocol)
+				{
+				case 0: 	  
+					protocol = "tcp";
+					break;
+#ifdef IPPROTO_SCTP
+				case IPPROTO_SCTP:
+					protocol = "sctp";
+					break;
+#endif
+				}
+				break;
+			case SOCK_DGRAM:
+				protocol = "udp";
+				break;
+			}
+		if (!protocol)
+		{
+			errno =  EPROTONOSUPPORT;
+			return 1;
+		}
+		if (sp = getservbyname(service, protocol))
+			ip_port = sp->s_port;
+	}
+	if (!ip_port)
+	{
+		errno = EADDRNOTAVAIL;
+		return EAI_SYSTEM;
+	}
+	if (!(ap = newof(0, struct addrinfo, 1, sizeof(struct sockaddr_in))))
+		return EAI_SYSTEM;
+	if (hint)
+		*ap = *hint;
+	ap->ai_family = hp->h_addrtype;
+	ap->ai_addrlen 	= sizeof(struct sockaddr_in);
+	ap->ai_addr = (struct sockaddr *)(ap+1);
+	ip = (struct sockaddr_in *)ap->ai_addr;
+	ip->sin_family = AF_INET;
+	ip->sin_port = ip_port;
+	ip->sin_addr.s_addr = ip_addr;
+	*addr = ap;
+	return 0;
+}
+
+static void
+freeaddrinfo(struct addrinfo* ap)
+{
+	if (ap)
+		free(ap);
+}
+
+#endif
+
+/*
+ * return <protocol>/<host>/<service> fd
+ */
+
+typedef int (*Inetintr_f)(int, void*);
+
+static int
+inetopen(const char* path, int server, Inetintr_f onintr, void* handle)
+{
+	register char*		s;
+	register char*		t;
+	int			fd;
+	int			oerrno;
+	struct addrinfo		hint;
+	struct addrinfo*	addr;
+	struct addrinfo*	p;
+
+	memset(&hint, 0, sizeof(hint));
+	hint.ai_family = PF_UNSPEC;
+	switch (path[0])
+	{
+#ifdef IPPROTO_SCTP
+	case 's':
+		if (path[1]!='c' || path[2]!='t' || path[3]!='p' || path[4]!='/')
+		{
+			errno = ENOTDIR;
+			return -1;
+		}
+		hint.ai_socktype = SOCK_STREAM;
+		hint.ai_protocol = IPPROTO_SCTP;
+		path += 5;
+		break;
+#endif
+	case 't':
+		if (path[1]!='c' || path[2]!='p' || path[3]!='/')
+		{
+			errno = ENOTDIR;
+			return -1;
+		}
+		hint.ai_socktype = SOCK_STREAM;
+		path += 4;
+		break;
+	case 'u':
+		if (path[1]!='d' || path[2]!='p' || path[3]!='/')
+		{
+			errno = ENOTDIR;
+			return -1;
+		}
+		hint.ai_socktype = SOCK_DGRAM;
+		path += 4;
+		break;
+	default:
+		errno = ENOTDIR;
+		return -1;
+	}
+	if (!(s = strdup(path)))
+		return -1;
+	if (t = strchr(s, '/'))
+	{
+		*t++ = 0;
+		if (streq(s, "local"))
+			s = "localhost";
+		fd = getaddrinfo(s, t, &hint, &addr);
+	}
+	else
+		fd = -1;
+	free(s);
+	if (fd)
+	{
+		if (fd != EAI_SYSTEM)
+			errno = ENOTDIR;
+		return -1;
+	}
+	oerrno = errno;
+	errno = 0;
+	fd = -1;
+	for (p = addr; p; p = p->ai_next)
+	{
+		/*
+		 * some api's don't take the hint
+		 */
+
+		if (!p->ai_protocol)
+			p->ai_protocol = hint.ai_protocol;
+		if (!p->ai_socktype)
+			p->ai_socktype = hint.ai_socktype;
+		if ((fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) >= 0)
+		{
+			do
+			{
+				if (server && !bind(fd, p->ai_addr, p->ai_addrlen) && !listen(fd, 5) || !server && !connect(fd, p->ai_addr, p->ai_addrlen))
+					goto done;
+			} while (errno == EINTR && (!onintr || !(*onintr)(fd, handle)));
+			close(fd);
+			fd = -1;
+		}
+	}
+ done:
+	freeaddrinfo(addr);
+	if (fd >= 0)
+		errno = oerrno;
+	return fd;
+}
+
 #else
-#   undef SOCKET
+
+#undef	O_SERVICE
+
 #endif
 
 struct fdsave
@@ -96,7 +308,6 @@ struct fdsave
 	int	save_fd;	/* saved file descriptor */
 	int	subshell;	/* saved for subshell */
 };
-
 
 static int  	subexcept(Sfio_t*, int, void*, Sfdisc_t*);
 static int  	eval_exceptf(Sfio_t*, int, void*, Sfdisc_t*);
@@ -370,6 +581,22 @@ int sh_close(register int fd)
 	return(r);
 }
 
+static int
+onintr(int fd, void* handle)
+{
+	Shell_t*	sh = (Shell_t*)handle;
+
+	if (sh->trapnote&SH_SIGSET)
+	{
+		close(fd);
+		sh_exit(SH_EXITSIG);
+		return -1;
+	}
+	if (sh->trapnote)
+		sh_chktrap();
+	return 0;
+}
+
 /*
  * Mimic open(2) with checks for pseudo /dev/ files.
  */
@@ -378,12 +605,6 @@ int sh_open(register const char *path, int flags, ...)
 	register int		fd = -1;
 	mode_t			mode;
 	char			*e;
-#ifdef SOCKET
-	int			type = -1;
-	int			prot = -1;
-	int			info = 0;
-	struct sockaddr_in	addr;
-#endif /* SOCKET */
 	va_list			ap;
 	va_start(ap, flags);
 	mode = (flags & O_CREAT) ? va_arg(ap, int) : 0;
@@ -395,6 +616,7 @@ int sh_open(register const char *path, int flags, ...)
 		return(-1);
 	}
 	if (path[0]=='/' && path[1]=='d' && path[2]=='e' && path[3]=='v' && path[4]=='/')
+	{
 		switch (path[5])
 		{
 		case 'f':
@@ -422,34 +644,17 @@ int sh_open(register const char *path, int flags, ...)
 						fd = 1;
 					break;
 				}
-#if defined(SOCKET) && defined(IPPROTO_SCTP)
-			else if (path[6]=='c' && path[7]=='t' && path[8]=='p' && path[9]=='/')
-			{
-				type = SOCK_STREAM;
-				prot = IPPROTO_SCTP;
-				info = 10;
-			}
-#endif
-			break;
-#ifdef SOCKET
-		case 't':
-			if (path[6]=='c' && path[7]=='p' && path[8]=='/')
-			{
-				type = SOCK_STREAM;
-				prot = 0;
-				info = 9;
-			}
-			break;
-		case 'u':
-			if (path[6]=='d' && path[7]=='p' && path[8]=='/')
-			{
-				type = SOCK_DGRAM;
-				prot = 0;
-				info = 9;
-			}
-			break;
-#endif
 		}
+#ifdef O_SERVICE
+		if (fd < 0)
+		{
+			if ((fd = inetopen(path+5, !!(flags & O_SERVICE), onintr, &sh)) < 0 && errno != ENOTDIR)
+				return -1;
+			if (fd >= 0)
+				goto ok;
+		}
+#endif
+	}
 	if (fd > 0)
 	{
 		if((mode=sh_iocheckfd(fd))==IOCLOSE)
@@ -462,42 +667,12 @@ int sh_open(register const char *path, int flags, ...)
 		if((fd=dup(fd))<0)
 			return(-1);
 	}
-#ifdef SOCKET
-	else if (type > 0 && str2inet(path+info, &addr))
-	{
-		if ((fd = socket(AF_INET, type, prot)) >= 0)
-		{
-			if(flags&O_SERVICE)
-			{
-				if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) || listen(fd,5))
-				{
-					close(fd);
-					fd = -1;
-				}
-			}
-			else while(connect(fd, (struct sockaddr*)&addr, sizeof(addr)))
-			{
-				if(errno==EINTR)
-				{
-					if(sh.trapnote&SH_SIGSET)
-					{
-						close(fd);
-						sh_exit(SH_EXITSIG);
-					}
-					if(sh.trapnote)
-						sh_chktrap();
-					continue;
-				}
-				close(fd);
-				fd = -1;
-				break;
-			}
-		}
-	}
-#endif /* SOCKET */
 	else while((fd = open(path, flags, mode)) < 0)
 		if(errno!=EINTR || sh.trapnote)
 			return(-1);
+#ifdef O_SERVICE
+ ok:
+#endif
 	flags &= O_ACCMODE;
 	if(flags==O_WRONLY)
 		mode = IOWRITE;
@@ -1238,80 +1413,6 @@ int sh_ioaccess(int fd,register int mode)
 	}
 	return(-1);
 }
-
-#ifdef SOCKET
-
-#ifndef INADDR_LOOPBACK
-#define INADDR_LOOPBACK		0x7f000001L
-#endif
-
-/*
- * convert string to sockaddr_in
- * 0 returned on error
- */
-
-static int str2inet(register const char *sp, struct sockaddr_in *addr)
-{
-	char		*proto = (char*)sp;
-	register int	n=0,c,v;
-	unsigned long	a=0;
-	unsigned short	p=0;
-
-	sp += 4;
-	if(memcmp(sp,"local/",6)==0)
-	{
-		a = INADDR_LOOPBACK;
-		n=4;
-		sp+=6;
-	}
-	else if(!isdigit(*sp))
-	{
-		struct hostent *hp;
-		char *cp = strchr(sp,'/');
-		*cp = 0;
-		hp = gethostbyname(sp);
-		*cp = '/';
-		if(!hp || hp->h_addrtype!=AF_INET || hp->h_length>sizeof(struct in_addr))
-			return 0;
-		a = (unsigned long)((struct in_addr*)hp->h_addr)->s_addr;
-		n=6;
-		sp = cp+1;
-		if(!isdigit(*sp))
-		{
-			struct servent *xp;
-			proto[3] = 0;
-			if(xp = getservbyname(sp,proto))
-			{
-				p = xp->s_port;
-				sp = "";
-			}
-			proto[3] = '/';
-		}
-	}
-	while(*sp)
-	{
-		v = 0;
-		while ((c = *sp++) >= '0' && c <= '9')
-			v = v * 10 + c - '0';
-		if (++n <= 4) a = (a << 8) | (v & 0xff);
-		else
-		{
-			if (c) return(0);
-			if (n <= 5)
-				a = htonl(a);
-			p = htons(v);
-			break;
-		}
-		if (c != '.' && c != '/') return(0);
-	}
-	memset((char*)addr, 0, sizeof(*addr));
-	addr->sin_family = AF_INET;
-	addr->sin_addr.s_addr = a;
-	addr->sin_port = p;
-	return(1);
-}
-#endif /* SOCKET */
-
 
 /*
  *  Handle interrupts for slow streams
