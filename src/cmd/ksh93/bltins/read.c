bb7ec166b8eaf8bb6f406c751b59d35370a3bf1b
Johnothan King <johnothanking@protonmail.com>
2008-12-12 20:31:15 -0500

    Version: 2008-12-12 ksh93t+
    Sourced from ast-open-history version 2008-12-12 and:
    8c80de8413fd803c45c4f1e22b79ec66228b392b15278888f8aa169195499c91  INIT.2008-12-12.tgz

--- src/cmd/ksh93/bltins/read.c
+++ src/cmd/ksh93/bltins/read.c
@@ -226,7 +226,7 @@ int sh_readline(register Shell_t *shp,char **names, int fd, int flags,long timeo
 	{
 		if(val= strchr(name,'?'))
 			*val = 0;
-		np = nv_open(name,shp->var_tree,NV_NOASSIGN|NV_VARNAME);
+		np = nv_open(name,shp->var_tree,NV_NOASSIGN|NV_VARNAME|NV_ARRAY);
 		if((flags&V_FLAG) && shp->ed_context)
 			((struct edit*)shp->ed_context)->e_default = np;
 		if(flags&A_FLAG)
